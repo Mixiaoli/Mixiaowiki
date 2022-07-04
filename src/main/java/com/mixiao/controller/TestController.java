4558cd6d8ada7e21e00c9f860434b27d646f4e37
Mixiaoli <2324598590@qq.com>
2022-07-04 16:58:09 +0800

    7-4 1.增加了自定义配置项-hello调用执行

--- src/main/java/com/mixiao/controller/TestController.java
+++ src/main/java/com/mixiao/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.mixiao.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller //返回一个页面
 public class TestController {
 
+    @Value("${test.hello}") //获取配置的值 然后下面hello调用
+    private String testHello;
+
     @RequestMapping("/hello") //定义URL路径 表示这个接口支持所以请求方式 POST GET...
     public String hello(){
-        return "hello world";
+        return "hello world" + testHello;
     }
 
     @PostMapping("/hello/post") //定义URL路径 表示这个接口支持所以请求方式 POST GET...
