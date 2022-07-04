ff9e4432854ea7990573089217a3690214239622
Mixiaoli <2324598590@qq.com>
2022-07-04 11:55:42 +0800

    7-4 init

---
+++ src/main/java/com/mixiao/MixiaowikiApplication.java
@@ -0,0 +1,13 @@
+package com.mixiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixiaowikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixiaowikiApplication.class, args);
+    }
+
+}
