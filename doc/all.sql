50cdd359742b0ce7103cb7dad734b0f4fa745e74
Mixiaoli <2324598590@qq.com>
2022-07-05 14:44:15 +0800

    7-5 集成Mybatis generator-config.xml,生成demo表代码测试

--- doc/all.sql
+++ doc/all.sql
@@ -7,6 +7,18 @@ create table `test`(
     primary key (`id`)
 )engine=innodb default charset =utf8mb4 comment='测试';
 insert into test(id, name, password) values (1,'cf',123456);
+
+
+#demo user测试表
+drop table if exists `demo`;
+create table `demo`(
+   `id` bigint not null comment 'id',
+   `name` varchar(50) comment  '名称',
+    primary key (`id`)
+)engine=innodb default charset =utf8mb4 comment='测试';
+insert into demo(id, name) values (1,'cs');
+
+
 #电子书表
 drop table if exists `ebook`;
 create table `ebook`(
