9381704ac8d71e1e85d872320a350ee894912513
Mixiaoli <2324598590@qq.com>
2022-07-06 14:50:37 +0800

    7-6 制作the-header组件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -11,7 +11,7 @@
         <template #title>
               <span>
                 <user-outlined />
-                subnav 111
+                subnav
               </span>
         </template>
         <a-menu-item key="1">option1</a-menu-item>
