f2fc00a85474a436ace72d5a9fa4187df0917d77
Mixiaoli <2324598590@qq.com>
2022-07-06 14:29:39 +0800

    7-6 引入Ant Design UI     首页路由开发     关闭vue/no-unused-components校验

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+//导入Ant
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+//集成第三方插件Vue都是在main.ts里配置
+
+createApp(App).use(store).use(router).use(Antd).mount('#app')
+
