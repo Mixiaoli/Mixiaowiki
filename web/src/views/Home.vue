a2487dd8f319e1d64bb9e6ead2aca928c3c38b22
Mixiaoli <2324598590@qq.com>
2022-07-06 16:41:48 +0800

    7-6 使用ref和reactive实现数据绑定 在页面显示

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -3,8 +3,6 @@
   <a-layout-sider width="200" style="background: #fff">
     <a-menu
         mode="inline"
-        v-model:selectedKeys="selectedKeys2"
-        v-model:openKeys="openKeys"
         :style="{ height: '100%', borderRight: 0 }"
     >
       <a-sub-menu key="sub1">
@@ -48,18 +46,35 @@
   <a-layout-content
       :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
   >
-    Content
+    <pre>{{ ebooks }}</pre>
+    <pre>{{ ebooks2 }}</pre>
   </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
-
+import { defineComponent,onMounted,ref,reactive,toRef } from 'vue';//写上onMounted VUE3.0 setup集成了 导入ref 做响应式数据
+import axios from 'axios';
 export default defineComponent({
   name: 'Home',
-  components: {
-  },
+  setup(){
+      console.log("setup");
+      const ebooks = ref();//响应式数据 获取的书籍实时反馈到页面上
+      const ebooks1 = reactive({books:[]});//json数据，返回给books这个数组
+
+      onMounted(()=> {
+        console.log("onMounted");
+        axios.get("http://localhost:8081/ebook/list?name=Vue").then((response) => {//获取后端接口数据
+          const data =response.data;
+          ebooks.value=data.content;
+          ebooks1.books= data.content;
+          console.log(response);
+        });
+      })
+    return {
+        ebooks,
+        ebooks2: toRef(ebooks1,"books")
+    }
+  }
 });
 </script>
