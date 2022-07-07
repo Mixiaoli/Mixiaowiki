8af85ec92973d60e88cea4d2137d2325cf202ead
Mixiaoli <2324598590@qq.com>
2022-07-07 13:46:54 +0800

    7-7 集成PgeHelper插件分页 讲mapper日志级别改为trace,打印所有sql日志,分页

--- src/main/java/com/mixiao/service/EbookService.java
+++ src/main/java/com/mixiao/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.mixiao.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.mixiao.domain.Ebook;
 import com.mixiao.domain.EbookExample;
 import com.mixiao.mapper.EbookMapper;
 import com.mixiao.req.EbookReq;
 import com.mixiao.resp.EbookResp;
 import com.mixiao.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,7 +18,7 @@ import java.util.List;
 
 @Service//需要声明服务层才会扫描到
 public class EbookService {
-
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);//把代码模板导入进来
     @Resource //jdk自带的注入 @Autowired spring自带的
     private EbookMapper ebookMapper;
 
@@ -25,7 +29,12 @@ public class EbookService {
             criteria.andNameLike("%" + req.getName() + "%");
         }
         //改返回值-实体类转换用for循环
+        PageHelper.startPage(1,3);//使用PageHelper分页插件 1页3条
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
         //单体
         /**List<EbookResp> respList = new ArrayList<>();
         for (Ebook ebook : ebookList){
