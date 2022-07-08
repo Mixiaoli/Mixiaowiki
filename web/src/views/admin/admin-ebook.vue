da251657bc29a6bd3e8516f56030acd230688a2d
Mixiaoli <2324598590@qq.com>
2022-07-08 18:42:36 +0800

    7-8 加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -275,10 +275,6 @@ export default defineComponent({
 
     onMounted(() => {
       handleQueryCategory();
-      handleQuery({
-        page:1,
-        size:pagination.value.pageSize
-      });
     });
     return {
       param,
