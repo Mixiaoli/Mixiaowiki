6c3b16d5b4124046263c398999dd752fec1c4c85
Mixiaoli <2324598590@qq.com>
2022-07-08 16:08:23 +0800

    7-8 分类表格按树形结构展示 增加了数组转换树形数据工具方法tool.ts

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -25,7 +25,7 @@
       <a-table
           :columns="columns"
           :row-key="record => record.id"
-          :data-source="categorys"
+          :data-source="level1"
           :loading="loading"
           :pagination="false"
       >
@@ -109,6 +109,18 @@ export default defineComponent({
       }
     ];
     /**
+     * 一级分类树，children属性就是二级分类
+     * [{
+     *   id: "",
+     *   name: "",
+     *   children: [{
+     *     id: "",
+     *     name: "",
+     *   }]
+     * }]
+     */
+    const  level1 =ref();
+    /**
      * 数据查询
      **/
     const handleQuery = () => {
@@ -118,6 +130,11 @@ export default defineComponent({
        const data = response.data;
         if (data.success){
           categorys.value = data.content;
+          console.log("原始数组：", categorys.value);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys.value, 0);//调用tool.ts做树形结构
+          console.log("树形结构：", level1);
         }else{
           message.error(data.message);
         }
@@ -179,7 +196,8 @@ export default defineComponent({
     });
     return {
       param,
-      categorys,//表格
+      level1,
+      //categorys,//表格
       columns,
       loading,
       handleQuery,
