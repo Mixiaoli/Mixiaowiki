350e0c97f848259a56b06b86ac4776ec748619f3
Mixiaoli <2324598590@qq.com>
2022-07-09 13:43:20 +0800

    7-9 添加文档Sql,并生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -61,3 +61,20 @@ insert into `category` (id, parent, name, sort) values(302,300,'服务器',302);
 insert into `category` (id, parent, name, sort) values(400,000,'开发工具',400);
 insert into `category` (id, parent, name, sort) values(401,400,'其他',401);
 insert into `category` (id, parent, name, sort) values(402,400,'集成开发',402);
+#文档表
+drop table if exists `doc`;
+create table `doc`(
+    `id` bigint not null comment 'id',
+    `ebook_id` bigint not null default  0 comment '电子书id',
+    `parent` bigint not null default 0 comment '父id',
+    `name` varchar(50) not null comment '名称',
+    `sort` int comment '顺序',
+    `view_count` int default 0 comment '阅读数',
+    `vote_count` int default 0 comment '点赞数',
+    primary key (`id`)
+)engine=innodb default charset =utf8mb4 comment='文档';
+
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count) values (1,1,0,'文档1',1,0,0);
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count) values (2,1,1,'文档1.1',1,0,0);
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count) values (3,1,0,'文档2',1,0,0);
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count) values (4,1,3,'文档2.1',1,0,0);
