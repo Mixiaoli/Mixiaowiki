b197156a6e318ca531625c1c930647a628fc070d
Mixiaoli <2324598590@qq.com>
2022-07-10 21:55:42 +0800

    7-10 完成用户表基本的增删改查功能

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/home.vue'
 import About from '../views/about.vue'
 import AdminEbook from '../views/admin/admin-ebook.vue'
+import AdminUser from '../views/admin/admin-user.vue'
 import Doc from '../views/doc.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
@@ -24,6 +25,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/doc',
     name: 'Doc',
     component: Doc
+  },
+  {
+    path: '/admin/user',
+    name: 'AdminUser',
+    component: AdminUser
   },
   {
     path: '/admin/ebook',
